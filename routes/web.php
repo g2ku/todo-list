897843e5aef8ff58c4f0041175cb0fadaef8abfb
ravil <141517347+g2ku@users.noreply.github.com>
2024-09-05 08:22:19 +0300

    add project

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
+Route::put('/tasks/{task}/complete', [TaskController::class, 'update'])->name('tasks.update');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+
+
+
